f9793cce366e3e1a0e72d3382b0f9c8b99343af9
MAY THAZIN HLAING <maythazinhlaing@MAY-2.local>
2025-03-03 22:00:10 +0900

    backend file edited

--- backend.tf
+++ backend.tf
@@ -7,8 +7,6 @@ terraform {
     profile      = "tf-state-handler"
     use_lockfile = true
     encrypt      = true
-    # access_key = data.vault_generic_secret.aws_creds.data["aws_access_key_id"]
-    # secret_key = data.vault_generic_secret.aws_creds.data["aws_secret_access_key"]
-  }
+      }
 }
 
